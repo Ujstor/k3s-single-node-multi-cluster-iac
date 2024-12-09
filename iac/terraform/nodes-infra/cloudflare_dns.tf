2bca558430ab20526c1fcf4f7bd21e90a7f19b56
Ujstor <astipan@gmail.com>
2024-12-09 22:40:26 +0100

    2. cluster bootstrap config

--- iac/terraform/nodes-infra/cloudflare_dns.tf
+++ iac/terraform/nodes-infra/cloudflare_dns.tf
@@ -34,14 +34,38 @@ module "cloudflare_record" {
       ttl     = 60
       proxied = false
     }
-    # wildcard_domain_k3s1 = {
-    #   zone_id = var.cloudflare_zone_id
-    #   name    = "*"
-    #   content = module.k3s_prod.server_info.k3s1-app.ip
-    #   type    = "A"
-    #   ttl     = 60
-    #   proxied = false
-    # }
+    portfolio_k3s1 = {
+      zone_id = var.cloudflare_zone_id
+      name    = "portfolio"
+      content = module.k3s_prod.server_info.k3s1-app.ip
+      type    = "A"
+      ttl     = 60
+      proxied = false
+    }
+    notes_k3s1 = {
+      zone_id = var.cloudflare_zone_id
+      name    = "notes"
+      content = module.k3s_prod.server_info.k3s1-app.ip
+      type    = "A"
+      ttl     = 60
+      proxied = false
+    }
+    working_hours_k3s1 = {
+      zone_id = var.cloudflare_zone_id
+      name    = "working-hours"
+      content = module.k3s_prod.server_info.k3s1-app.ip
+      type    = "A"
+      ttl     = 60
+      proxied = false
+    }
+    wordpress_k3s1 = {
+      zone_id = var.cloudflare_zone_id
+      name    = "wordpress"
+      content = module.k3s_prod.server_info.k3s1-app.ip
+      type    = "A"
+      ttl     = 60
+      proxied = false
+    }
   }
   depends_on = [module.k3s_prod]
 }
