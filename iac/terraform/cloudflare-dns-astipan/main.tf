5ecc640ab9e92662c14dc14a7662d3e85a0d15d7
Ujstor <astipan@gmail.com>
2024-12-13 23:59:17 +0100

    migrate apps

--- iac/terraform/cloudflare-dns-astipan/main.tf
+++ iac/terraform/cloudflare-dns-astipan/main.tf
@@ -7,17 +7,41 @@ module "cloudflare_record" {
       name    = "@"
       content = data.hcloud_server.k3s1.ipv4_address
       type    = "A"
-      ttl     = 60
-      proxied = false
+      ttl     = 1
+      proxied = true
     }
-    # portfolio_CNAME = {
-    #   zone_id = var.cloudflare_zone_id
-    #   name    = "www"
-    #   content = "astipan.com"
-    #   type    = "CNAME"
-    #   ttl     = 60
-    #   proxied = false
-    # }
+    portfolio_CNAME = {
+      zone_id = var.cloudflare_zone_id
+      name    = "www"
+      content = "astipan.com"
+      type    = "CNAME"
+      ttl     = 1
+      proxied = true
+    }
+    working-hours = {
+      zone_id = var.cloudflare_zone_id
+      name    = "working-hours"
+      content = data.hcloud_server.k3s1.ipv4_address
+      type    = "A"
+      ttl     = 1
+      proxied = true
+    }
+    todo = {
+      zone_id = var.cloudflare_zone_id
+      name    = "todo"
+      content = data.hcloud_server.k3s1.ipv4_address
+      type    = "A"
+      ttl     = 1
+      proxied = true
+    }
+    notes-flask = {
+      zone_id = var.cloudflare_zone_id
+      name    = "notes-flask"
+      content = data.hcloud_server.k3s1.ipv4_address
+      type    = "A"
+      ttl     = 1
+      proxied = true
+    }
   }
 }
 
--- iac/terraform/nodes-infra/cloudflare_dns.tf
+++ iac/terraform/nodes-infra/cloudflare_dns.tf
@@ -7,72 +7,48 @@ module "cloudflare_record" {
       name    = "api.k3s0"
       content = module.k3s_prod.server_info.k3s0-ops.ip
       type    = "A"
-      ttl     = 60
-      proxied = false
+      ttl     = 1
+      proxied = true
     }
     wildcard_k3s0 = {
       zone_id = var.cloudflare_zone_id
       name    = "*.k3s0"
       content = module.k3s_prod.server_info.k3s0-ops.ip
       type    = "A"
-      ttl     = 60
-      proxied = false
+      ttl     = 1
+      proxied = true
     }
     kube_api_k3s1 = {
       zone_id = var.cloudflare_zone_id
       name    = "api.k3s1"
       content = module.k3s_prod.server_info.k3s1-app.ip
       type    = "A"
-      ttl     = 60
-      proxied = false
+      ttl     = 1
+      proxied = true
     }
     wildcard_k3s1 = {
       zone_id = var.cloudflare_zone_id
       name    = "*.k3s1"
       content = module.k3s_prod.server_info.k3s1-app.ip
       type    = "A"
-      ttl     = 60
-      proxied = false
+      ttl     = 1
+      proxied = true
     }
-    portfolio_k3s1 = {
+    plausible-analytics = {
       zone_id = var.cloudflare_zone_id
-      name    = "portfolio"
+      name    = "analytics"
       content = module.k3s_prod.server_info.k3s1-app.ip
       type    = "A"
-      ttl     = 60
-      proxied = false
-    }
-    notes_k3s1 = {
-      zone_id = var.cloudflare_zone_id
-      name    = "notes"
-      content = module.k3s_prod.server_info.k3s1-app.ip
-      type    = "A"
-      ttl     = 60
-      proxied = false
-    }
-    working_hours_k3s1 = {
-      zone_id = var.cloudflare_zone_id
-      name    = "working-hours"
-      content = module.k3s_prod.server_info.k3s1-app.ip
-      type    = "A"
-      ttl     = 60
-      proxied = false
+      ttl     = 1
+      proxied = true
     }
     wordpress_k3s1 = {
       zone_id = var.cloudflare_zone_id
       name    = "wordpress"
       content = module.k3s_prod.server_info.k3s1-app.ip
       type    = "A"
-      ttl     = 60
-      proxied = false
-    }
-    todo_k3s1 = {
-      zone_id = var.cloudflare_zone_id
-      name    = "todo"
-      content = module.k3s_prod.server_info.k3s1-app.ip
-      type    = "A"
-      ttl     = 60
-      proxied = false
+      ttl     = 1
+      proxied = true
     }
   }
   depends_on = [module.k3s_prod]
